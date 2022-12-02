09de9f15e57b6b42012925cdc923002f772a76a1
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2022-12-02 14:07:47 -0600

    setup Apollo and graphql in frontEnd app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,34 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { setContext } from "@apollo/client/link/context";
+import {ApolloClient,InMemoryCache,ApolloProvider, createHttpLink,} from "@apollo/client";
+
+// get to graphql for querie and mutation
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// pretty much as boilerplate as exercise in class
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
-  return (
+  return ( 
+  <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +41,7 @@ function App() {
         </Switch>
       </>
     </Router>
+  </ApolloProvider>
   );
 }
 
